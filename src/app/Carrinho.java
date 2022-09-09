e6e4f277ad73dc30b40a2d21bf69a433ef7c5710
Tiago <70555750+tiagoluis86@users.noreply.github.com>
2022-09-09 15:22:19 -0300

    FIX classe carrinho

--- src/app/Carrinho.java
+++ src/app/Carrinho.java
@@ -9,7 +9,7 @@ public class Carrinho {
     private String user;
     List<String> produtos_carrinho;
 
-    public Carrinho(int valor_carrinho, String usuario, List produtos_carrinho){
+    public Carrinho(int valor_carrinho, String user, List produtos_carrinho){
         this.valor_carrinho = valor_carrinho;
         this.user = user;
         this.produtos_carrinho = produtos_carrinho;
@@ -18,9 +18,9 @@ public class Carrinho {
 
     public static Carrinho adicionarCarrinho(){
         int valor_carrinho = 0;
-        String usuario = "";
+        String user = "";
         List<String> produtos_carrinho = new ArrayList<>();
-        return new Carrinho(valor_carrinho, usuario, produtos_carrinho);
+        return new Carrinho(valor_carrinho, user, produtos_carrinho);
     }
 
     public int getValorCarrinho(){
